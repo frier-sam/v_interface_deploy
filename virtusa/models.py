7db0348648bd049dfe8dd8033f7c33b32af3afd3
Dipsikha Das Gupta <dipsikhadg@virtusa.com>
2019-08-19 14:48:30 +0530

    '3rd_prototype'

--- virtusa/models.py
+++ virtusa/models.py
@@ -20,7 +20,7 @@ class Ml_models(models.Model):
     name = models.CharField(max_length=50,null=False,unique=True,blank=False)
     summary = models.TextField(null=False)
     content = models.TextField(null=False)
-    extras = models.TextField(null= True)
+    extras = models.TextField(null= True,blank=True)
     image = models.ImageField(upload_to='images/',default='images/default.jpg')
     url = models.URLField(null=True,blank=True)
     inputs_csv = models.FileField(upload_to="inputs/")
@@ -28,6 +28,7 @@ class Ml_models(models.Model):
     category = models.ManyToManyField(category,default=0)
     locations = models.CharField(max_length=100,null=True,blank=True)
     date_time = models.DateTimeField(default=datetime.now)
+    active = models.BooleanField(default=True)
 
 
 
